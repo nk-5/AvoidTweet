42685844644073d95e64679e4a9f39c3fcabac3f
keigo-nakagawa <keigo.nakagawa@dena.com>
2019-12-19 20:06:45 +0900

    update

--- AvoidTweet/GameScene.swift
+++ AvoidTweet/GameScene.swift
@@ -14,11 +14,92 @@ class GameScene: SKScene {
     private var twitter : SKSpriteNode?
     private var background : SKSpriteNode?
     private var spinnyNode : SKShapeNode?
+    private var obstacle : SKSpriteNode?
     
     override func didMove(to view: SKView) {
         
-        physicsWorld.gravity =  CGVector(dx: 0, dy: -1)
+        physicsWorld.gravity =  CGVector(dx: 0, dy: -0.5)
+        physicsBody?.isDynamic = false
+        physicsBody?.allowsRotation = false
+//        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
+        drawBackground()
+        drawTwitter()
+        drawObstacle()
+
+        let wall = CGRect(x: frame.origin.x,
+                          y: frame.origin.y + twitter!.frame.height/2,
+                          width: frame.width,
+                          height: frame.height)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: wall)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+        
+//        let touch:UITouch = touches.anyObject() as! UITouch
+//        let positionInScene = touch.location(in: self)
+//        let touchedNode = twitter(positionInScene)
+//
+//        if let name = touchedNode.name
+//        {
+//            if name == "pineapple"
+//            {
+//                print("Touched")
+//            }
+//        }
+        
+        twitter?.run(SKAction.moveTo(y: twitter!.position.y + 150, duration: 0.5))
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    private func drawBackground() {
         background = SKSpriteNode(imageNamed: "background")
         guard let background = background else { return }
         background.size = size
@@ -50,100 +131,29 @@ class GameScene: SKScene {
         
         addChild(background)
         addChild(backgroundMore)
-        
+    }
+    
+    private func drawTwitter() {
         let f1 = SKTexture.init(imageNamed: "twitterbird1")
         let f2 = SKTexture.init(imageNamed: "twitterbird2")
         let frames: [SKTexture] = [f1, f2]
-
+        
         twitter = SKSpriteNode(imageNamed: "twitterbird1")
         guard let twitter = twitter else { return }
         
         twitter.zPosition = 1
-        twitter.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-
+        twitter.position = CGPoint(x: self.frame.midX - frame.width/4, y: self.frame.midY)
+        
         let animation = SKAction.animate(with: frames, timePerFrame: 0.1)
         twitter.run(SKAction.repeatForever(animation))
         
         twitter.physicsBody?.isDynamic = true
-        twitter.physicsBody = SKPhysicsBody(texture: twitter.texture!, size: twitter.size)
+        twitter.physicsBody?.allowsRotation = false
+        twitter.physicsBody = SKPhysicsBody(rectangleOf: twitter.frame.size)
         addChild(twitter)
-
-
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-        
-//        let touch:UITouch = touches.anyObject() as! UITouch
-//        let positionInScene = touch.location(in: self)
-//        let touchedNode = twitter(positionInScene)
-//
-//        if let name = touchedNode.name
-//        {
-//            if name == "pineapple"
-//            {
-//                print("Touched")
-//            }
-//        }
-        
-        twitter?.run(SKAction.moveTo(y: twitter!.position.y + 100, duration: 0.5))
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    private func drawObstacle() {
+       obstacle = SKSpriteNode(texture: SKTexture, color: <#T##UIColor#>, size: <#T##CGSize#>)
     }
 }
---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+ruby '2.3.7'
+
+gem 'cocoapods', '1.8.4'
