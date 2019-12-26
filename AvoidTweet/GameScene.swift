df23d2863076d89affde7629107d87fadb4e1e51
keigo-nakagawa <keigo.nakagawa@dena.com>
2019-12-26 19:53:56 +0900

    finish

--- AvoidTweet/GameScene.swift
+++ AvoidTweet/GameScene.swift
@@ -17,7 +17,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var tweet1 : SKLabelNode?
     private var tweet2 : SKLabelNode?
     private var tweet3 : SKLabelNode?
-    
+    private var terminate : SKLabelNode?
+    private var restart : SKSpriteNode?
+
     private var terminated: Bool = false
     
     override func didMove(to view: SKView) {
@@ -25,22 +27,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity =  CGVector(dx: 0, dy: -0.5)
         physicsWorld.contactDelegate = self
         physicsBody?.isDynamic = false
-        physicsBody?.allowsRotation = false
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsBody?.affectedByGravity = false
+        physicsBody?.allowsRotation = false
         
         drawBackground()
         drawTwitter()
-        
+   
         tweet1 = SKLabelNode(text: "ツイート！！！")
-        tweet2 = SKLabelNode(text: "ツイート！！！")
-        tweet3 = SKLabelNode(text: "ツイート！！！")
-        drawObstacle(tweet: tweet1!)
-        drawObstacle(tweet: tweet2!)
-        drawObstacle(tweet: tweet3!)
+        tweet2 = SKLabelNode(text: "なんでやねん！！！")
+        tweet3 = SKLabelNode(text: "ラーメン食いたい🍜！！！")
+        drawObstacle(tweet: tweet1!, duration: 3.5)
+        drawObstacle(tweet: tweet2!, duration: 4.5)
+        drawObstacle(tweet: tweet3!, duration: 5.5)
     }
-    
-    
+
     func touchDown(atPoint pos : CGPoint) {
     }
     
@@ -51,25 +52,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        background?.removeAllActions()
-        backgroundMore?.removeAllActions()
-        twitter?.removeAllActions()
-        tweet1?.removeAllActions()
-        tweet2?.removeAllActions()
-        tweet3?.removeAllActions()
         terminated = true
+        self.scene?.isPaused = true
         drawTerminate()
+        drawRestart()
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            self.touchDown(atPoint: t.location(in: self))
+            let targets = self.nodes(at: t.location(in: self))
+            for node in targets {
+                if node.name == "restart" {
+                    self.reset()
+                }
+            }
+        }
+        
         if terminated {
-           return
+            return
         }
         let diff = frame.origin.y * -1 - twitter!.position.y
         let jump = diff < 150 ? diff : 150
         twitter?.run(SKAction.moveTo(y: twitter!.position.y + jump, duration: 0.5))
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -146,7 +152,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(twitter)
     }
     
-    private func drawObstacle(tweet: SKLabelNode) {
+    private func drawObstacle(tweet: SKLabelNode, duration: TimeInterval) {
         tweet.position = CGPoint(x: size.width, y: CGFloat.random(in: self.frame.origin.y + tweet.frame.height..<(self.frame.origin.y * -1) - tweet.frame.height))
         tweet.fontColor = .black
         tweet.fontSize = 40
@@ -160,7 +166,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         tweet.run(
             SKAction.repeatForever(
                 SKAction.sequence([
-                    SKAction.moveTo(x: -size.width, duration: 5.5),
+                    SKAction.moveTo(x: -size.width, duration: duration),
                     SKAction.run({
                         tweet.position = CGPoint(x: self.size.width,
                                                   y: CGFloat.random(in: self.frame.origin.y + tweet.frame.height..<(self.frame.origin.y * -1) - tweet.frame.height))
@@ -173,13 +179,54 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     private func drawTerminate() {
-       let terminate = SKLabelNode(text: "本日はここまで")
-        terminate.position = CGPoint(x: 0.5, y: 0.5)
-        terminate.fontColor = .red
-        terminate.fontSize = 80
-        terminate.zPosition = 2
+        if let terminate = terminate {
+            terminate.isHidden = false
+            return
+        }
+        terminate = SKLabelNode(text: "本日はここまで")
+        terminate?.position = CGPoint(x: 0.5, y: 0.5)
+        terminate?.fontColor = .red
+        terminate?.fontSize = 80
+        terminate?.zPosition = 2
         
-        self.addChild(terminate)
+        self.addChild(terminate!)
+    }
+    
+    private func drawRestart() {
+        if let restart = restart {
+            restart.isHidden = false
+            return
+        }
+        restart = SKSpriteNode(imageNamed: "restart")
+        guard let restart = restart else { return }
+        
+        restart.zPosition = 2
+        let y = frame.origin.y * -1
+        restart.position = CGPoint(x: 0.5, y: y - 100)
+        restart.name = "restart"
+        addChild(restart)
+    }
+    
+    private func reset() {
+        twitter?.position = CGPoint(x: self.frame.midX - frame.width/4, y: self.frame.midY)
+//        tweet1?.position = CGPoint(x: size.width, y: (tweet1?.frame.origin.y)!)
+//        tweet2?.position = CGPoint(x: size.width, y: (tweet2?.frame.origin.y)!)
+//        tweet3?.position = CGPoint(x: size.width, y: (tweet3?.frame.origin.y)!)
+        removeChildren(in: [
+            tweet1!,
+            tweet2!,
+            tweet3!
+        ])
+        tweet1 = SKLabelNode(text: "ツイート！！！")
+        tweet2 = SKLabelNode(text: "なんでやねん！！！")
+        tweet3 = SKLabelNode(text: "ラーメン食いたい🍜！！！")
+        drawObstacle(tweet: tweet1!, duration: 3.5)
+        drawObstacle(tweet: tweet2!, duration: 4.5)
+        drawObstacle(tweet: tweet3!, duration: 5.5)
+        restart?.isHidden = true
+        terminate?.isHidden = true
+        terminated = false
+        self.scene?.isPaused = false
     }
 }
 
